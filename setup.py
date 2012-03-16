db40ba80f91255ee10e3412a9c9df6c9d179d64f
Ruslan Spivak <ruslan.spivak@gmail.com>
2012-03-15 23:14:21 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read(*rel_names):
 
 setup(
     name='slimit',
-    version='0.6.1dev',
+    version='0.6.1',
     url='http://slimit.org',
     license='MIT',
     description='SlimIt - JavaScript minifier',
